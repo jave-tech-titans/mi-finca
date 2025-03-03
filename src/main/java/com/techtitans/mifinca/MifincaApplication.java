32c4ae574db83757b48d38bcf37f0cda44013b79
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2025-03-03 17:08:56 -0500

    Initial commit

---
+++ src/main/java/com/techtitans/mifinca/MifincaApplication.java
@@ -0,0 +1,13 @@
+package com.techtitans.mifinca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MifincaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MifincaApplication.class, args);
+	}
+
+}
