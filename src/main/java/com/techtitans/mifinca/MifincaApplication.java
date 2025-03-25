5ae2cc255724731718778f6946f97684403c9391
diegobermudez03 <bermudezjimenezjuandiego@gmail.com>
2025-03-25 12:47:35 -0500

    SOme public modifiers removed from tests, modified so it only loads .env in dev env

--- src/main/java/com/techtitans/mifinca/MifincaApplication.java
+++ src/main/java/com/techtitans/mifinca/MifincaApplication.java
@@ -9,8 +9,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class MifincaApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		if(System.getenv("CRYPT_KEY") == null){
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		}
 		SpringApplication.run(MifincaApplication.class, args);
 	}
 }
